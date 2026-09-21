5d335bbc6d437a474d1c14f02b448fa50c6a4b18
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2026-09-22 03:08:31 +0900

    test: keep config validation focused on domain constraints

--- src/test/java/net/okocraft/bluemapmarkers/config/ConfigTest.java
+++ src/test/java/net/okocraft/bluemapmarkers/config/ConfigTest.java
@@ -174,16 +174,6 @@ class ConfigTest {
     }
 
     @Test
-    void testLoadRejectsNegativeDecimalWorldGuardUpdateLimit() {
-        var yaml = MINIMAL_CONFIG.replace(
-                "    default:\n      render-setting:",
-                "    default:\n      update-limit: -1.0\n      render-setting:"
-        );
-
-        Assertions.assertThrows(ConfigurateException.class, () -> this.load(yaml));
-    }
-
-    @Test
     void testLoadRejectsNonPositiveSeparationSize() {
         var yaml = MINIMAL_CONFIG.replace(
                 "      separation-setting: {}",
@@ -218,16 +208,6 @@ class ConfigTest {
         var yaml = MINIMAL_CONFIG.replace(
                 "  marker-set:\n    name: Border\n",
                 "  marker-set:\n    name: Border\n  outline-color: { invalid: value }\n"
-        );
-
-        Assertions.assertThrows(ConfigurateException.class, () -> this.load(yaml));
-    }
-
-    @Test
-    void testLoadRejectsListColor() {
-        var yaml = MINIMAL_CONFIG.replace(
-                "  marker-set:\n    name: Border\n",
-                "  marker-set:\n    name: Border\n  outline-color: [ '#ff0000ff' ]\n"
         );
 
         Assertions.assertThrows(ConfigurateException.class, () -> this.load(yaml));
