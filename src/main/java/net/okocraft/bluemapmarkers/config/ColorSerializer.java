e6d92eb022fc2d7b8309ebfcc84369260f4364ed
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2026-09-22 03:08:12 +0900

    refactor: simplify color serialization

--- src/main/java/net/okocraft/bluemapmarkers/config/ColorSerializer.java
+++ src/main/java/net/okocraft/bluemapmarkers/config/ColorSerializer.java
@@ -1,49 +1,30 @@
 package net.okocraft.bluemapmarkers.config;
 
 import de.bluecolored.bluemap.api.math.Color;
-import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.serialize.ScalarSerializer;
 import org.spongepowered.configurate.serialize.SerializationException;
 import org.spongepowered.configurate.serialize.TypeSerializer;
 
-import java.lang.reflect.Type;
+final class ColorSerializer {
 
-final class ColorSerializer implements TypeSerializer<Color> {
-
-    static final ColorSerializer INSTANCE = new ColorSerializer();
+    static final ScalarSerializer<Color> INSTANCE = TypeSerializer.of(
+            Color.class,
+            (color, acceptedType) -> "#%02x%02x%02x%02x".formatted(
+                    color.getRed(),
+                    color.getGreen(),
+                    color.getBlue(),
+                    Math.round(color.getAlpha() * 255)
+            ),
+            value -> {
+                try {
+                    return new Color(value.toString());
+                } catch (NumberFormatException e) {
+                    throw new SerializationException(e);
+                }
+            }
+    );
 
     private ColorSerializer() {
-    }
-
-    @Override
-    public Color deserialize(Type type, ConfigurationNode node) throws SerializationException {
-        if (node.isMap() || node.isList()) {
-            throw new SerializationException(node, type, "Color must be a scalar value");
-        }
-
-        var value = node.rawScalar();
-        if (value == null) {
-            throw new SerializationException(node, type, "No scalar color value present");
-        }
-
-        try {
-            return new Color(value.toString());
-        } catch (NumberFormatException e) {
-            throw new SerializationException(node, type, e);
-        }
-    }
-
-    @Override
-    public void serialize(Type type, Color color, ConfigurationNode node) throws SerializationException {
-        if (color == null) {
-            node.raw(null);
-            return;
-        }
-
-        node.set("#%02x%02x%02x%02x".formatted(
-                color.getRed(),
-                color.getGreen(),
-                color.getBlue(),
-                Math.round(color.getAlpha() * 255)
-        ));
+        throw new UnsupportedOperationException();
     }
 }
