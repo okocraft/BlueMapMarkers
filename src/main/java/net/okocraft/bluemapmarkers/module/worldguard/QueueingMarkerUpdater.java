33588b75ac4d6c3f35de89eb0c708b8081a1a511
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2026-09-21 22:39:41 +0900

    perf: release WorldGuard update buffers after each cycle

--- src/main/java/net/okocraft/bluemapmarkers/module/worldguard/QueueingMarkerUpdater.java
+++ src/main/java/net/okocraft/bluemapmarkers/module/worldguard/QueueingMarkerUpdater.java
@@ -1,56 +1,52 @@
 package net.okocraft.bluemapmarkers.module.worldguard;
 
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.managers.RegionManager;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import it.unimi.dsi.fastutil.objects.ObjectSet;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 class QueueingMarkerUpdater {
 
     private final ObjectSet<RenderedRegionInfo> renderedRegions = new ObjectOpenHashSet<>();
     private final int limit;
 
-    private Iterator<ProtectedRegion> regionIterator;
+    private Queue<String> regionIdQueue;
     private ObjectSet<RenderedRegionInfo> removedRegions;
 
     QueueingMarkerUpdater(int limit) {
         this.limit = limit;
     }
 
-    void beginUpdate(@NotNull Collection<ProtectedRegion> regions) {
-        if (!this.isFinished()) {
-            throw new IllegalStateException("The previous update is still in progress");
-        }
-
-        this.regionIterator = regions.iterator();
-        this.removedRegions = new ObjectOpenHashSet<>(this.renderedRegions);
-    }
-
-    boolean doUpdate(@NotNull WorldGuardRenderer renderer) {
-        if (this.regionIterator == null) {
-            throw new IllegalStateException("beginUpdate must be called before doUpdate");
+    boolean doUpdate(@NotNull WorldGuardRenderer renderer, @NotNull RegionManager regionManager) {
+        if (this.regionIdQueue == null || this.regionIdQueue.isEmpty()) {
+            this.regionIdQueue = new ArrayDeque<>(regionManager.getRegions().keySet());
+            this.removedRegions = new ObjectOpenHashSet<>(this.renderedRegions);
         }
 
         for (int i = 0; i < this.limit; i++) {
-            if (!this.regionIterator.hasNext()) {
+            var id = this.regionIdQueue.poll();
+
+            if (id == null) {
                 this.finishUpdate(renderer);
                 return true;
             }
 
-            var region = this.regionIterator.next();
+            var region = regionManager.getRegion(id);
 
-            var info = renderer.renderRegion(region);
+            if (region != null) {
+                var info = renderer.renderRegion(region);
 
-            if (info != null) {
-                this.renderedRegions.add(info);
-                this.removedRegions.remove(info);
+                if (info != null) {
+                    this.renderedRegions.add(info);
+                    this.removedRegions.remove(info);
+                }
             }
         }
 
-        if (!this.regionIterator.hasNext()) {
+        if (this.regionIdQueue.isEmpty()) {
             this.finishUpdate(renderer);
             return true;
         }
@@ -61,10 +57,11 @@ class QueueingMarkerUpdater {
     private void finishUpdate(@NotNull WorldGuardRenderer renderer) {
         renderer.removeRegions(this.removedRegions);
         this.renderedRegions.removeAll(this.removedRegions);
-        this.removedRegions.clear();
+        this.regionIdQueue = null;
+        this.removedRegions = null;
     }
 
     boolean isFinished() {
-        return this.regionIterator == null || !this.regionIterator.hasNext();
+        return this.regionIdQueue == null || this.regionIdQueue.isEmpty();
     }
 }
