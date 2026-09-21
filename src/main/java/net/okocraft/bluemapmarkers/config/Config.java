f3d90ae79e9155d45804a9f456b3ecd912a0cfc3
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2026-09-22 02:37:09 +0900

    refactor: load config with configurate

--- src/main/java/net/okocraft/bluemapmarkers/config/Config.java
+++ src/main/java/net/okocraft/bluemapmarkers/config/Config.java
@@ -1,25 +1,39 @@
 package net.okocraft.bluemapmarkers.config;
 
-import dev.siroshun.codec4j.api.decoder.Decoder;
-import dev.siroshun.codec4j.api.decoder.object.FieldDecoder;
-import dev.siroshun.codec4j.api.decoder.object.ObjectDecoder;
-import dev.siroshun.codec4j.api.error.DecodeError;
-import dev.siroshun.codec4j.io.yaml.YamlIO;
-import dev.siroshun.jfun.result.Result;
 import org.jetbrains.annotations.NotNull;
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
-public record Config(@NotNull WorldBorderSetting worldBorderSetting,
-                     @NotNull WorldGuardSetting worldGuardSetting) {
+@ConfigSerializable
+public final class Config {
 
-    private static final Decoder<Config> DECODER = ObjectDecoder.create(
-            Config::new,
-            FieldDecoder.required("world-border-setting", WorldBorderSetting.DECODER),
-            FieldDecoder.required("world-guard-setting", WorldGuardSetting.DECODER)
-    );
+    private WorldBorderSetting worldBorderSetting = new WorldBorderSetting();
+    private WorldGuardSetting worldGuardSetting = new WorldGuardSetting();
 
-    public static @NotNull Result<Config, DecodeError> loadFromYamlFile(@NotNull Path filepath) {
-        return YamlIO.DEFAULT.decodeFrom(filepath, DECODER);
+    public Config() {
+    }
+
+    public Config(@NotNull WorldBorderSetting worldBorderSetting, @NotNull WorldGuardSetting worldGuardSetting) {
+        this.worldBorderSetting = worldBorderSetting;
+        this.worldGuardSetting = worldGuardSetting;
+    }
+
+    public @NotNull WorldBorderSetting worldBorderSetting() {
+        return this.worldBorderSetting;
+    }
+
+    public @NotNull WorldGuardSetting worldGuardSetting() {
+        return this.worldGuardSetting;
+    }
+
+    public static @NotNull Config loadFromYamlFile(@NotNull Path filepath) throws IOException {
+        var loader = YamlConfigurationLoader.builder()
+                .path(filepath)
+                .build();
+
+        return loader.load().require(Config.class);
     }
 }
